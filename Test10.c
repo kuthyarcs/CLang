b1f44746d3927cd41ffd8d84e7ad0fd032426fe0
kuthyarcs <kuthyar.cs@sode-edu.in>
2025-01-23 15:55:43 +0530

    Variable assignment

---
+++ Test10.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/*Write a C program to store different data like studentId, studentAge, StudentFee,StudentGrade about a college student and print the same*/
+main() {
+	int id=1;
+	int age=26;
+	int fees=5000;
+	char grade='B';
+	printf("ID=%d\nAGE=%d\nFEES=%d\nGRADE=%c\n",id,age,fees,grade);
+	return 0;
+}
---
+++ Test11.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+int main(){
+	int x,y,z;
+	x=y=z=100;
+	printf("%d",x+y+z);
+	return 0;
+}
