df30ab743873f1421bd601a9c6a45f5d21e5188d
kuthyarcs <kuthyar.cs@sode-edu.in>
2025-01-23 13:30:55 +0530

    Print float, char and ASCII

---
+++ Test5.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main(){
+	int num1=15;
+	float num2=6.6689;
+	char char1='G';
+	printf("%d\n", num1);
+	printf("%2f\n", num2);
+	printf("ASCII value of %c = %d\n", char1, char1);
+	printf("%d\n", char1);
+}
