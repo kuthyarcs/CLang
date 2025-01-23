44d70ba6960604efd42d00350055cfa4a58db5a3
kuthyarcs <kuthyar.cs@sode-edu.in>
2025-01-23 16:26:33 +0530

    Practice

---
+++ Test14.c
@@ -0,0 +1,8 @@
+//Write a C program to calculate and output the total cost of a number of items
+#include <stdio.h>
+int main(){
+	int items=50;
+	float cost=6.50
+	float totalCost;
+	totalCost=items*cost;
+}
