0660df3f9e862a05a07dcb3374df0f3c5769a04d
kuthyarcs <kuthyar.cs@sode-edu.in>
2025-01-24 12:49:27 +0530

    While loop, Do While loop

---
+++ Test28.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+//print numbers from 1 to 10
+int main(){
+	int num1=1;
+	int num2=10;
+	while(num1<=num2){
+		printf(num1);
+		num1++;
+	}
+	return 0;
+}
---
+++ Test29.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+//print numbers from 1 to 10
+int main(){
+	int num1=1;
+	int num2=10;
+	printf("Hello1\n");
+	while(num1<=num2){
+		printf("%d\n", num1);
+		num1++;
+	}
+	printf("Hello2");
+	return 0;
+}
