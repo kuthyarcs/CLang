67e5a095b5050abd2ecd3125f2da6843d75dc880
kuthyarcs <kuthyar.cs@sode-edu.in>
2025-01-24 11:39:22 +0530

    Eligible to vpte

---
+++ Test22.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+//If age is >18, print "eligible to vote"
+int main(){
+	int age=9;
+	if(age>18){
+		printf("You are eligible to vote");
+	}
+	else {
+		printf("You are not eligible to vote");
+	}
+}
