d7242bd5e8ea33de49cc41104780cb3d2df2428e
kuthyarcs <kuthyar.cs@sode-edu.in>
2025-01-24 10:49:42 +0530

    Add files via upload

---
+++ Test17.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main(){
+	float result1=(float)5/2;
+	printf("%f\n",result1);
+	int num1=10;
+	int num2=8;
+	float result2=(float)num1/num2;
+	printf("%f\n",result2);
+	return 0;
+	
+}
