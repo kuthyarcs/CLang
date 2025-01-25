a3a579f7b25d6d312757ca0cd42d4565cb459336
kuthyarcs <kuthyar.cs@sode-edu.in>
2025-01-25 10:30:43 +0530

    Introduce escape character(\) before the double quote
    "We are the so-called \"Vikings\" from the north";

---
+++ Test54.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+//Trying to print thi linse = "We are the so-called "Vikings" from the north";
+int main(){
+	char txt[]="We are the so-called \"Vikings\" from the north";
+	printf("%s",txt);
+	return 0;
+}
