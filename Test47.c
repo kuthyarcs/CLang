9a6e89cf600d98de27535ddc966244f2db37be64
kuthyarcs <kuthyar.cs@sode-edu.in>
2025-01-25 09:42:16 +0530

    Check vowel or not

---
+++ Test47.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+int main(){
+	char char1='p';
+	if(char1=='a'){
+		printf("It is a vowel");
+	}else if(char1=='e'){
+		printf("It is a vowel");
+	}else if(char1=='i'){
+		printf("It is a vowel");
+	}else if(char1=='o'){
+		printf("It is a vowel");
+	}else if(char1=='u'){
+		printf("It is a vowel");
+	}else{
+		printf("'%c' is not a vowel", char1);
+	}
+	return 0;
+}
